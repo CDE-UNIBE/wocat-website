dec986129492f2cff8070f81ae63a43829d3458d
Eraldo Energy <eraldo@eraldo.org>
2016-07-14 19:59:10 +0200

    Updated media teaser context.

--- wocat/cms/blocks.py
+++ wocat/cms/blocks.py
@@ -208,23 +208,35 @@ class MediaChooserBlock(blocks.ChooserBlock):
 
 class MediaTeaserBlock(StructBlock):
     media = MediaChooserBlock(required=True)
+    image_position = ChoiceBlock(
+        choices=[
+            ('top', _('Top')),
+            ('left', _('Left')),
+            ('right', _('Right')),
+        ],
+        required=False,
+    )
 
     def get_context(self, value):
         media = value.get('media')
         page = media.detail_page
         file = media.file
+        image_position = value.get('image_position')
         return {
             'href': page.url if page else file.url,
             'title': media.title,
             'description': media.description,
             'author': media.author,
+            'readmorelink': {'text': _('Detail page') if page else _('Download')},
             'imgsrc': media.teaser_image.get_rendition('max-1200x1200').url if media.teaser_image else '',
+            'imgpos': image_position,
+            'mediastyle': True,
         }
 
     class Meta:
         icon = 'fa fa-file-o'
         label = _('Media Teaser')
-        template = 'widgets/media.html'
+        template = 'widgets/teaser.html'
 
 
 TEASER_BLOCKS = [
