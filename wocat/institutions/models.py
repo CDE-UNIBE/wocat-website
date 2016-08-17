04c8feda2295947ce5a47ab4bc3a82052a9a5abd
Eraldo Energy <eraldo@eraldo.org>
2016-08-17 20:53:54 +0200

    Updated the institution model.

--- wocat/institutions/models.py
+++ wocat/institutions/models.py
@@ -1,6 +1,9 @@
+from django.conf import settings
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
+from django_countries.fields import CountryField
 from wagtail.wagtailadmin.edit_handlers import FieldPanel
+from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
 from wagtail.wagtailsnippets.models import register_snippet
 
 
@@ -10,9 +13,33 @@ class Institution(models.Model):
         max_length=255,
         unique=True,
     )
+    abbreviation = models.CharField(
+        verbose_name=_('Abbreviation'),
+        max_length=255,
+        unique=True,
+    )
+    url = models.URLField(
+        verbose_name=_('Url'),
+        blank=True,
+    )
+    country = CountryField(
+        blank=True
+    )
+    logo = models.ForeignKey(
+        'wagtailimages.Image',
+        verbose_name=_('Logo'),
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
 
     panels = [
         FieldPanel('name'),
+        FieldPanel('abbreviation'),
+        FieldPanel('url'),
+        FieldPanel('country'),
+        ImageChooserPanel('logo'),
     ]
 
     class Meta:
