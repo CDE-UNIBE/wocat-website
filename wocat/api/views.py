e52bc1675cdf1a66cade78c1eb82bcebfe2ed8a4
Eraldo Energy <eraldo@eraldo.org>
2016-08-17 21:04:55 +0200

    Updated the institution api resource.

--- wocat/api/views.py
+++ wocat/api/views.py
@@ -1,12 +1,24 @@
 from rest_framework import viewsets, routers
 
+from wocat.institutions.models import Institution
+from wocat.institutions.serializers import InstitutionSerializer
 from wocat.users.models import User
 from wocat.users.serializers import UserSerializer
+
+router = routers.DefaultRouter()
 
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
-router = routers.DefaultRouter()
+
 router.register(r'users', UserViewSet)
+
+
+class InstitutionViewSet(viewsets.ModelViewSet):
+    queryset = Institution.objects.all()
+    serializer_class = InstitutionSerializer
+
+
+router.register(r'institutions', InstitutionViewSet)
