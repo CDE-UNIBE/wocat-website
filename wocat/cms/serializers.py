85691c48821ab2f297eada44bb0d66f4b287f7d8
Sebastian Manger <s.manger@gmx.ch>
2016-10-03 16:30:14 +0200

    49: Basic proof of concept for map * return data for all types (project, country, region) * simple frontend demo (js)

--- wocat/cms/serializers.py
+++ wocat/cms/serializers.py
@@ -17,7 +17,7 @@ class GeoJsonMixin:
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.geojson, self.country_keys = self.get_geojson()
+        self.geojson, self.country_keys = self.load_geojson()
 
     @lru_cache(maxsize=32)
     def get_country_geojson(self, country: str) -> dict:
@@ -25,7 +25,7 @@ class GeoJsonMixin:
             return self.geojson['features'][self.country_keys[country]]
         # else what? log error and display in template? how is logging handled?
 
-    def get_geojson(self) -> tuple:
+    def load_geojson(self) -> tuple:
         """
         Get a tuple of two elements:
         - python object of the defined geojson file
@@ -44,32 +44,44 @@ class GeoJsonMixin:
                 cache.set(self.filename, (geojson, country_keys))
         return geojson, country_keys
 
+    def get_geojson(self, obj) -> list:
+        raise NotImplementedError('The field "geojson" is required (frontend).')
+
 
 class ProjectSerializer(GeoJsonMixin, serializers.HyperlinkedModelSerializer):
     url = serializers.URLField(source='full_url')
-    countries = serializers.SerializerMethodField()
+    geojson = serializers.SerializerMethodField()
 
     class Meta:
         model = ProjectPage
-        fields = ('url', 'title', 'countries')
+        fields = ('url', 'title', 'geojson')
 
-    def get_countries(self, obj: ProjectPage) -> list:
+    def get_geojson(self, obj: ProjectPage) -> list:
         countries = ['ALB', 'DEU', 'CAN']
         return [self.get_country_geojson(country) for country in countries]
 
 
-class CountrySerializer(serializers.HyperlinkedModelSerializer):
+class CountrySerializer(GeoJsonMixin, serializers.HyperlinkedModelSerializer):
     url = serializers.URLField(source='full_url')
-    code = serializers.CharField(source='country.alpha3')
+    code = serializers.CharField(source='country.code')
+    geojson = serializers.SerializerMethodField()
 
     class Meta:
         model = CountryPage
-        fields = ('url', 'title', 'code')
+        fields = ('url', 'title', 'code', 'geojson')
+
+    def get_geojson(self, obj: CountryPage) -> list:
+        return self.get_country_geojson(obj.country.code)
 
 
-class RegionSerializer(serializers.HyperlinkedModelSerializer):
+class RegionSerializer(GeoJsonMixin, serializers.HyperlinkedModelSerializer):
     url = serializers.URLField(source='full_url')
+    geojson = serializers.SerializerMethodField()
 
     class Meta:
         model = RegionPage
-        fields = ('url', 'title', 'country_codes')
+        fields = ('url', 'title', 'country_codes', 'geojson')
+
+    def get_geojson(self, obj: RegionPage) -> list:
+        countries = ['ARG', 'ARM']
+        return [self.get_country_geojson(country) for country in countries]
