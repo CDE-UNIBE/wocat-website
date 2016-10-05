97b42deff3b22bc5ce0541c47d63981c02aeab52
Eraldo Energy <eraldo@eraldo.org>
2016-10-05 15:20:07 +0200

    Added memorandum and year to institution model.

--- wocat/institutions/models.py
+++ wocat/institutions/models.py
@@ -1,9 +1,13 @@
 from django.conf import settings
+from django.core.validators import MaxValueValidator
 from django.db import models
+from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
 from django_countries.fields import CountryField
 from wagtail.wagtailadmin.edit_handlers import FieldPanel
 from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
+from wagtail.wagtailsearch import index
+from django.core.validators import MaxValueValidator
 
 
 class Institution(models.Model):
@@ -21,8 +25,23 @@ class Institution(models.Model):
         verbose_name=_('Url'),
         blank=True,
     )
+    year = models.PositiveIntegerField(
+        _('Year'),
+        default=timezone.now().year,
+        validators=[MaxValueValidator(100)]
+    )
     country = CountryField(
         blank=True
+    )
+    # contact_person = models.ForeignKey(
+    #     settings.AUTH_USER_MODEL,
+    #     verbose_name=_('Contact person'),
+    #     on_delete=models.SET_NULL,
+    #     null=True
+    # )
+    memorandum = models.BooleanField(
+        verbose_name=_('Memorandum signed'),
+        default=False
     )
     logo = models.ForeignKey(
         'wagtailimages.Image',
@@ -37,9 +56,22 @@ class Institution(models.Model):
         FieldPanel('name'),
         FieldPanel('abbreviation'),
         FieldPanel('url'),
+        FieldPanel('year'),
         FieldPanel('country'),
+        # FieldPanel('contact_person'),
+        FieldPanel('memorandum'),
         ImageChooserPanel('logo'),
     ]
+
+    search_fields = [
+        index.SearchField('name'),
+        index.SearchField('abbreviation'),
+        index.SearchField('content'),
+        index.FilterField('year'),
+        # index.FilterField('contact_person'),
+        index.FilterField('country'),
+    ]
+
 
     class Meta:
         verbose_name = _('Institution')
