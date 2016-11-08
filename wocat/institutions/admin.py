cb640a533cbab9ab1fc3f29412daca104e0d4148
Eraldo Energy <eraldo@eraldo.org>
2016-11-08 23:47:21 +0100

    Added the institution model to the admin view.

--- wocat/institutions/admin.py
+++ wocat/institutions/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Institution
+
+
+@admin.register(Institution)
+class InstitutionAdmin(admin.ModelAdmin):
+    pass
