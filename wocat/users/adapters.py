c85f1b71a02fb76d3eb9f5a4e400046f552320ac
Eraldo Energy <eraldo@eraldo.org>
2016-11-15 17:33:53 +0100

    Fixed signup workflow.

--- wocat/users/adapters.py
+++ wocat/users/adapters.py
@@ -8,12 +8,6 @@ class AccountAdapter(DefaultAccountAdapter):
     def is_open_for_signup(self, request):
         return getattr(settings, 'ACCOUNT_ALLOW_REGISTRATION', True)
 
-    # def send_confirmation_mail(self, request, emailconfirmation, signup):
-    #     user = emailconfirmation.email_address.user
-    #     user.is_active = False
-    #     user.save()
-    #     super().send_confirmation_mail(request, emailconfirmation, signup)
-
 
 class SocialAccountAdapter(DefaultSocialAccountAdapter):
     def is_open_for_signup(self, request, sociallogin):
