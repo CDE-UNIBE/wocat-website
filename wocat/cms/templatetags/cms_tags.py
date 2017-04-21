47fd35683fca4ca0cf48e0bf2916a0f3d1b0bea4
Simon Liedtke <sl@sinnwerkstatt.com>
2017-04-21 18:22:43 +0200

    Minor fixes

--- wocat/cms/templatetags/cms_tags.py
+++ wocat/cms/templatetags/cms_tags.py
@@ -274,23 +274,3 @@ class Overlay(InclusionTag):
             'lead': richtext(header.get('content')) if header.get('content') else '',
             'noimage': not header.get('images'),
         }
-
-
-'''
-    Registers render_streamfield tag to add current page context to all block templates.
-    Usage: {% render_streamfield a_streamfield %}
-
-    Author: github.com/mgd020
-'''
-
-
-@register.simple_tag(takes_context=True)
-def render_streamfield(context, value):
-    def get_context(self, value):
-        return dict(context.flatten(), **{
-            'self': value,
-            self.TEMPLATE_VAR: value,
-        })
-
-    Block.get_context = get_context
-    return str(value)
