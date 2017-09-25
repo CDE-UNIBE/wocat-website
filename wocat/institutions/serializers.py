f6d1e76487969e6e8a331037752de049b3475f93
Sebastian Manger <s.manger@gmx.ch>
2017-09-25 13:34:09 +0200

    1703 Add extended (requested) attributes

--- wocat/institutions/serializers.py
+++ wocat/institutions/serializers.py
@@ -6,21 +6,18 @@ from wocat.institutions.models import Institution
 class InstitutionSerializer(serializers.HyperlinkedModelSerializer):
     url = serializers.HyperlinkedIdentityField(view_name='institutions:detail', lookup_field='slug')
     external_url = serializers.URLField(source='url')
-    # country = serializers.URLField(source='country.get_absolute_url')
-    # country = serializers.PrimaryKeyRelatedField(
-    #     many=False,
-    #     # lookup_field='code'
-    # )
-    # country = serializers.HyperlinkedRelatedField(
-    #     view_name='countries:detail',
-    #     lookup_field='slug',
-    #     read_only=True
-    # )
-
     country = serializers.PrimaryKeyRelatedField(many=False, read_only=True)
+    logo = serializers.SerializerMethodField()
 
     class Meta:
         model = Institution
         fields = [
-            'id', 'url', 'external_url', 'name', 'abbreviation', 'year', 'country', 'contact_person', 'memorandum'
+            'id', 'url', 'external_url', 'name', 'abbreviation', 'year',
+            'country', 'contact_person', 'memorandum', 'logo'
         ]
+
+    def get_logo(self, obj):
+        if obj.logo and 'request' in self.context:
+            img_path = obj.logo.get_rendition('max-1200x1200').url
+            return self.context['request'].build_absolute_uri(img_path)
+        return ''
