f0194dd79a7009c1e6e203d0732074f9669dfdff
Sebastian Manger <s.manger@gmx.ch>
2017-11-13 16:01:33 +0100

    1704 Add newsletter status to admin for a 'list of all subscribers'

--- wocat/users/admin.py
+++ wocat/users/admin.py
@@ -117,7 +117,7 @@ class UserAdmin(AuthUserAdmin):
         }),
     )
 
-    list_display = ('email', 'first_name', 'last_name', 'is_staff')
-    list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
+    list_display = ('email', 'first_name', 'last_name', 'is_staff', 'is_active', 'newsletter', )
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups', 'newsletter', )
     search_fields = ('email', 'first_name', 'last_name')
     ordering = ('first_name', 'last_name', 'email')
